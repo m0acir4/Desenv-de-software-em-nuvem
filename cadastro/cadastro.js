758af6be8fabc5c9ff313872dd4df0f96958879f
m0acir4 <moacircadmiel42@gmail.com>
2025-03-21 19:46:53 -0300

    meu sexto commit

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -16,6 +16,7 @@ document.getElementById('cadastroForm').addEventListener('submit', async (event)
         const result = await response.json();
     if (response.ok) {
         alert(result.message);
+        window.location.href = '/Login/login.html';
         document.getElementById('email').value = '';
         document.getElementById('senha').value = '';
     } else {
